351d48bcd3783e52793faa9701b8fa082ef2d1b9
alvareztuamiguelangel <alvareztuamiguelangel.com>
2021-12-20 09:07:41 -0500

    si se pudo

---
+++ src/repositorio/Repositorio.java
@@ -0,0 +1,27 @@
+
+package repositorio;
+
+/**
+ *
+ * @author angel
+ */
+public class Repositorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("MIGUEL");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
